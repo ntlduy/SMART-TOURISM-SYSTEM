bf1dcadeb902456e5664d204937be8865036b6bf
Billie <24127262@student.hcmus.edu.vn>
2025-12-04 12:47:21 +0700

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+﻿import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import ShopDetail from "./components/ShopDetail";
+import './App.css';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                {/* Route động: :id sẽ nhận bất kỳ số nào */}
+                <Route path="/shops/:id" element={<ShopDetail />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
